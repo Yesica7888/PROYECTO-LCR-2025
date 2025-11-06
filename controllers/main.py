b2daafae7f68bfb3d297f348d82bc76d34840825
Yes <yesicarodriguez_097@hotmail.com>
2025-11-06 17:00:27 -0500

    Modulo reporte pdf plantilla
    modulo sin acceso a datos, todos los odulos del menu funcionales

--- controllers/main.py
+++ controllers/main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
 from models.deteccion import getTotalDeteccion  
 from models.deteccion_diagnostico import getDetDiaResumen,get_total_riesgo_bajo,get_total_riesgo_alto,get_total_riesgo_moderado #O_O
-
+#para el reporte
+from reportlab.pdfgen import canvas
+import os
 
 main= Blueprint('main', __name__ )
 
@@ -36,9 +38,24 @@ def detecciones():
     print("Registros:", type(registros), len(registros))
     return render_template('plantilla/deteccion.html',titulos=columns,registros=registros,)
 
-@main.route('/reporte')
+
+@main.route('/reportepdf')
 def reporte():
-    return render_template('plantilla/reporte-pdf.html')
+    # Ruta del archivo PDF
+    filename = "reporte.pdf"
+    path = os.path.join("static", "reportes", filename)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    # Crear PDF básico si no existe
+    if not os.path.exists(path):
+        c = canvas.Canvas(path)
+        c.drawString(100, 750, "PDF vacío de prueba")
+        c.save()
+
+    # Renderizar la plantilla HTML del visor
+    return render_template('plantilla/reporte-pdf.html', pdf_file=filename)
+
+    
 
 
 
